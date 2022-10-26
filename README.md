b3308722eae3f4aff1e0be2cc7124811c00705e7
Francisco J. Moreno <franmorfe@gmail.com>
2022-10-26 16:35:45 +0200

    First commit. Postgres DB configuration

---
+++ README.md
@@ -0,0 +1,20 @@
+<p align="center">
+  <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="200" alt="Nest Logo" /></a>
+</p>
+
+
+# Teslo-Shop API
+1. Clonar proyecto
+2. Instalar la aplicación
+```
+yarn install
+```
+3. Renombrar ```.env.template``` a ```.env``` y cambiar las variables de entorno por las propias
+5. Levantar la base de datos
+```
+docker-compose up -d
+```
+6. Iniciar aplicación en modo desarrollo
+```
+yarn start:dev
+```
---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      entities: [],
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
